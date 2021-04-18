9136c387673f5bc0f1c0a5174898fef578c34b34
MD Arora <mdarora0001@gmail.com>
2021-04-18 14:47:37 +0530

    previous & next buttons activated

--- script.js
+++ script.js
@@ -1,19 +1,70 @@
+const coverImg = document.querySelector("img");
+const title = document.getElementById("title");
+const artist = document.getElementById("artist");
 const playBtn = document.getElementById("play");
 const nextBtn = document.getElementById("next");
 const prevBtn = document.getElementById("prev");
 const music = document.querySelector("audio");
 
+const songs = [
+    {
+        title:"Aadat",
+        artist:"Sucha Yaar",
+        coverImage:"./img/jacinto-1.jpg",
+        filePath:"./music/Aadat Sucha Yaar.mp3"
+    },
+    {
+        title:"Adhi adhi raat",
+        artist:"Bilal Saeed",
+        coverImage:"./img/jacinto-2.jpg",
+        filePath:"./music/Adhi-Adhi-Raat-Bilal-Saeed.mp3"
+    },
+    {
+        title:"Akhian",
+        artist:"Happy Raikoti",
+        coverImage:"./img/jacinto-3.jpg",
+        filePath:"./music/Akhian - Happy Raikoti.mp3"
+    },
+];
+
 let isPlaying = false;
-playBtn.addEventListener("click", ()=>{
-    if (isPlaying){
-        isPlaying = false;
-        music.pause();
-        playBtn.setAttribute("title", "Play");
-        playBtn.classList.replace("fa-pause", "fa-play");
-    } else{
-        isPlaying = true;
-        music.play();
-        playBtn.setAttribute("title", "Pause");
-        playBtn.classList.replace("fa-play", "fa-pause");
-    }
-})
+function playSong(){
+    isPlaying = true;
+    music.play();
+    playBtn.setAttribute("title", "Pause");
+    playBtn.classList.replace("fa-play", "fa-pause");
+}
+function pauseSong(){
+    isPlaying = false;
+    music.pause();
+    playBtn.setAttribute("title", "Play");
+    playBtn.classList.replace("fa-pause", "fa-play");
+}
+
+function loadSong(index){
+    coverImg.src = songs[index].coverImage;
+    music.src = songs[index].filePath;
+    title.textContent = songs[index].title;
+    artist.textContent = songs[index].artist;
+}
+
+
+let songIndex = 0;
+function prevSong(){
+    songIndex--;
+    songIndex = songIndex < 0 ? songs.length - 1 : songIndex;
+    loadSong(songIndex);
+    playSong();
+}
+function nextSong(){
+    songIndex++;
+    songIndex = songIndex > songs.length - 1  ? 0 : songIndex;
+    loadSong(songIndex);
+    playSong();
+}
+
+loadSong(songIndex);
+
+playBtn.addEventListener("click", ()=> isPlaying ? pauseSong() : playSong());
+prevBtn.addEventListener("click", prevSong);
+nextBtn.addEventListener("click", nextSong);
